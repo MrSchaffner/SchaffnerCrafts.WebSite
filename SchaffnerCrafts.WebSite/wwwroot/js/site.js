ff8f2a5571b9a90f3a16b0e6cd1fca0dc3b694f3
GenoSophy <62560010+MrSchaffner@users.noreply.github.com>
2022-07-11 18:41:42 -0700

    init

--- SchaffnerCrafts.WebSite/wwwroot/js/site.js
+++ SchaffnerCrafts.WebSite/wwwroot/js/site.js
@@ -2,3 +2,28 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
+
+//animation
+
+
+function myMove() {
+
+
+    let id = null;
+    const elem = document.getElementById("animate");
+    let pos = 0;
+
+    clearInterval(id);
+    id = setInterval(frame, 500); //this does the animation
+
+    function frame() {
+        if (pos >= 350) {
+            clearInterval(id);
+
+        } else {
+            pos=pos+1;
+            elem.style.top = pos +"px";
+            elem.style.left = pos + "px";
+        }
+    }
+}
